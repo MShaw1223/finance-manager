443ec8798e54da312380014995d029a0d5a86379
miller <millershaw61@gmail.com>
2024-05-31 21:36:07 +0100

    Feat: Compares existing users with new
    Use a count of the exisisting users compared with the details of the new entered user before signup sent to db

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,7 +1,7 @@
 import { extractBody } from "@/utils/extractBody";
 import { NextRequest, NextResponse } from "next/server";
 import sqlstring from "sqlstring";
-import { Pool } from "@neondatabase/serverless";
+import { Pool, QueryResult } from "@neondatabase/serverless";
 
 export async function POST(req: NextRequest) {
   try {
@@ -9,20 +9,26 @@ export async function POST(req: NextRequest) {
       connectionString: process.env.DATABASE_URL,
     });
     const body = await extractBody(req);
-    const { username, user_password, email } = body;
-    const sqlStatement = sqlstring.format(
-      "insert into users (username, user_password, email) values (?, ?, ?)",
+    const { username, user_password, email }: { [key: string]: string } = body;
+    const checkQuery = sqlstring.format(
+      "select count(*) as checker from users where username = ? and user_password = ? and email = ?",
       [username, user_password, email]
     );
-    const uidQuery = sqlstring.format(
-      "select uid from users where username = ? AND user_password = ? AND email = ?",
-      [username, user_password, email]
-    );
-    await pool.query(sqlStatement);
-    const done = await pool.query(uidQuery);
-    const uid = done.rows[0].uid;
-    await pool.end();
-    return NextResponse.json({ uid }, { status: 200 });
+    const check = await pool.query(checkQuery);
+    if (parseInt(check.rows[0].checker) > 0) {
+      return NextResponse.json("User already exists", { status: 403 });
+    } else {
+      const sqlStatement = sqlstring.format(
+        "insert into users (username, user_password, email) values (?, ?, ?) returning uid",
+        [username, user_password, email]
+      );
+      const newUser: QueryResult<{ uid: number }> = await pool.query(
+        sqlStatement
+      );
+      const uid = newUser.rows[0].uid;
+      await pool.end();
+      return NextResponse.json({ uid }, { status: 200 });
+    }
   } catch (error) {
     return NextResponse.json(`Error in login route: ${error}`, { status: 500 });
   }
