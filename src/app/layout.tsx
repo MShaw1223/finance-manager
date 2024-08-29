974d9b9a7526c94c6651606712e48513d7b11023
miller <millershaw61@gmail.com>
2024-08-29 01:45:22 +0100

    Style: Globals retry and meta tag
    Space between comma and user-scalable improves readability; Retry globals.css minimum font size in the varying inputs to prevent zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <head>
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1,user-scalable=0"
+            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
           />
         </head>
         <body>
